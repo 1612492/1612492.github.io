cee347bef103bb3acd38b4ba42497aba034317d8
1612492 <39008689+1612492@users.noreply.github.com>
2023-06-14 12:39:47 +0700

    chore: playground

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-
-import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react()]
 });
